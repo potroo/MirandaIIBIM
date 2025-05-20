151bbb1e0ddace2477fbb8b193083dcc927237e0
joão vitor <joaovitor.miranda1210@gmail.com>
2025-05-20 15:48:29 -0300

    tb

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,11 @@ function App() {
     <>
       <br />
       <br />
-      <h1 className='titulo'>Nome do Blog</h1>
+      <h1 className='titulo'>Check list</h1>
       <br />
       <br />
-      <Box produto={"A Sociedade Esportiva Palmeiras é um dos clubes mais tradicionais e vitoriosos do futebol brasileiro. Sua história começou em 26 de agosto de 1914, quando um grupo de imigrantes italianos fundou o clube com o nome de Palestra Italia, na cidade de São Paulo. A ideia era criar uma equipe que representasse a comunidade italiana no Brasil e que promovesse a cultura e os valores dos imigrantes por meio do esporte. Nos primeiros anos, o Palestra Italia se destacou rapidamente no cenário paulista, conquistando seu primeiro título estadual em 1920. Durante as décadas de 1920 e 1930, o clube continuou crescendo, se tornando uma das principais forças do futebol de São Paulo."}/>
-      <br />
-      <Box produto={"A Sociedade Esportiva Palmeiras é um dos clubes mais tradicionais e vitoriosos do futebol brasileiro. Sua história começou em 26 de agosto de 1914, quando um grupo de imigrantes italianos fundou o clube com o nome de Palestra Italia, na cidade de São Paulo. A ideia era criar uma equipe que representasse a comunidade italiana no Brasil e que promovesse a cultura e os valores dos imigrantes por meio do esporte. Nos primeiros anos, o Palestra Italia se destacou rapidamente no cenário paulista, conquistando seu primeiro título estadual em 1920. Durante as décadas de 1920 e 1930, o clube continuou crescendo, se tornando uma das principais forças do futebol de São Paulo."}/>
+      <Box produto={""}/>
+    
     </>
   )
 }
--- src/components/Boxtexto.jsx
+++ src/components/Boxtexto.jsx
@@ -3,15 +3,11 @@ import './Boxtexto.css'
 function Box( {produto} ) {
     return (
         <>
+        <div className="top"></div>
             <div className='Box'>
-                <div className='Pessoa'>
-                    <div className='circulo'></div>
-                    <h2 className='pessoatxt'> Pessoa</h2>
-                </div>
                 <div className='Texto'>
                     <p> {produto} </p>
                 </div>
-                <div className='datahora'><p><strong> 12/04/2025 12:02 </strong></p></div>
             </div>
         </>
     )
