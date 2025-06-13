7dc7d55e44158bbf62e16a4e6e999abfdf0afb89
Bernardo Maia <bblagesmaia@gmail.com>
2025-06-13 11:50:17 -0300

    13/06

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,27 @@ let pessoas =[]
 function App() {
 const [ nome, setNome] = useState("")
 const[ idade, setIdade ] = useState("")
+const[ materia, setMateria ] = useState("")
+let newArray = array.filter( item => materia===item.materia)
+
 
 const handleClick = () => {
   pessoas.push({id:pessoas.length, nome, idade})
   console.log(pessoas)
   setNome("")
+  setIdade("")
 }
 const handleNome = (e) => {
   setNome(e.target.value)
+
 }
 const handleIdade = (e) => {
   setIdade(e.target.value)
+  
 } 
+const handleMateria = (e) => {
+  setMateria(e.target.value)
+}
   return (
     <>
     <div className='card'>
@@ -29,7 +38,7 @@ const handleIdade = (e) => {
       <h1 className='titulo'>Check list</h1>
       <br />
       <br />
-      <BoxTexto produto={pessoas} handleClick={handleClick} handleNome={handleNome} handleIdade={handleIdade} nome={nome} idade={idade}/>
+      <BoxTexto produto={pessoas} handleClick={handleClick} handleNome={handleNome} handleIdade={handleIdade} nome={nome} idade={idade} handleMateria={handleMateria} materia={materia}/>
     
     </>
   )
--- src/components/Boxtexto.jsx
+++ src/components/Boxtexto.jsx
@@ -1,33 +1,51 @@
 import './Boxtexto.css'
 
-function BoxTexto( {handleClick, handleNome, handleIdade, nome, idade, produto} ) {
-    return (
-        <>
-        <div className="top">
+function BoxTexto({ handleClick, handleNome, handleIdade, nome, idade, produto, handleMateria, materia }) {
+  return (
+    <>
+      <div className="top">
         <input type="text"
-        placeholder='digite aqui'
-        onChange={handleNome}
-        value={nome}
+          placeholder='digite aqui'
+          onChange={handleNome}
+          value={nome}
         />
-      <input type="number" 
-        placeholder='digite aqui'
-        onChange={handleIdade}
-        value={idade}/>
+        <input type="number"
+          placeholder='digite aqui'
+          onChange={handleIdade}
+          value={idade} />
 
-      < button onClick={handleClick}>clique aqui</button></div>   
+        <label htmlFor="materias">Matérias</label>
 
-            <div className='Box'>
+        <select onChange={handleMateria}>
 
-            {produto.map(e=>(
-            <div className="Box2" key={e.nome}>
+          <option value="Matematica">Matematica</option>
+          <option value="português">português</option>
+          <option value="biologia">biologia</option>
+          <option value="Quimica">Quimica</option>
+          <option value="Fisica">Fisica</option>
+          <option value="Historia">Historia</option>
+          <option value="Georafia">Georafia</option>
+
+        </select>
+
+
+        < button onClick={handleClick}>clique aqui</button>
+      </div >
+
+
+
+      <div className='Box'>
+
+        {produto.map(e => (
+          <div className="Box2" key={e.nome}>
             <p>nome: {e.nome}</p>
             <p>idade: {e.idade}</p>
-      </div>
+          </div>
 
-      ))}
-            </div>
-        </>
-    )
+        ))}
+      </div>
+    </>
+  )
 }
 
 export default BoxTexto
